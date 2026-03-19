2e3392e96d6b1cf079e8a39d9131639783989cbc
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2026-03-19 18:46:26 -0400

    - Changed the company logo. - Mobile menu done. - Generally improved the header design.

--- src/js/header/mobile-menu-manager.js
+++ src/js/header/mobile-menu-manager.js
@@ -1,14 +1,13 @@
 const mobileMenu = document.getElementById("mobile-menu");
 
 function showMobileMenu() {
-  mobileMenu.style.visibility = "visible";
-  mobileMenu.classList.remove("animate__fadeOutRight");
-  mobileMenu.classList.add("animate__fadeInRight");
+  mobileMenu.classList.remove("animate__pullUp");
+  mobileMenu.classList.add("animate__pullDown");
 }
 
 function hideMobileMenu() {
-  mobileMenu.classList.remove("animate__fadeInRight");
-  mobileMenu.classList.add("animate__fadeOutRight");
+  mobileMenu.classList.remove("animate__pullDown");
+  mobileMenu.classList.add("animate__pullUp");
 }
 
 export { showMobileMenu, hideMobileMenu };
