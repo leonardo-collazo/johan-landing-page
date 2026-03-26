0d65f3eba8ba917c7b0553b2e6a91a7331a73da5
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2026-03-26 15:31:14 -0400

    Added animation to hero section content.

--- src/js/language/change-language-icon.js
+++ src/js/language/change-language-icon.js
@@ -3,13 +3,13 @@ import { loadLanguageIcon } from "./load-language-icon.js";
 const languageIcon = document.getElementById("language-icon");
 
 function zoomInLanguageIcon() {
-  languageIcon.classList.remove("animate__zoomOut--language-icon");
-  languageIcon.classList.add("animate__zoomIn--language-icon");
+  languageIcon.classList.remove("animate__zoomOut");
+  languageIcon.classList.add("animate__zoomIn");
 }
 
 function zoomOutLanguageIcon() {
-  languageIcon.classList.remove("animate__zoomIn--language-icon");
-  languageIcon.classList.add("animate__zoomOut--language-icon");
+  languageIcon.classList.remove("animate__zoomIn");
+  languageIcon.classList.add("animate__zoomOut");
 }
 
 function changeLanguageIcon() {
